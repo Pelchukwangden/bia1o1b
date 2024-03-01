e7274d218b50743abe46295f1190389ba192bd9d
Pelchukwangden <03230190.gcbs@rub.edu.bt>
2024-03-01 12:02:39 +0600

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+varaiable1 = 10#integer
+# it is like x = 10
+
+nane = 'dechen'#string
+name2='dechen2'
+
+score = 10.1 #float/ decimal
+didHeWin = True #boolean
+
+print(type(name2))
+print(varaiable1)
+# we have to know types of data as integer cant be compared to str
