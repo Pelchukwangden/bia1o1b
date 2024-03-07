f766142fbafbd78cb513eefec874a2f0b9a75967
Pelchukwangden <03230190.gcbs@rub.edu.bt>
2024-03-08 05:01:28 +0600

    mycoolName

---
+++ coolName.py
@@ -0,0 +1,17 @@
+result = ""
+nameTest = "Pelden"
+for x in nameTest:
+    if x == "P":
+        result += "1"
+    elif x == "e":
+        result += "2"
+    elif x == "l":
+        result += "3"
+    elif x == "d":
+        result += "4"
+    elif x == "n":
+        result += "5"
+    else:
+        result = result + x
+
+print (result)
