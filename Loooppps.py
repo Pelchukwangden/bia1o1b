dfff64d5fbb1bd92dcc5eec69e5654444aa5e9d3
Pelchukwangden <03230190.gcbs@rub.edu.bt>
2024-03-14 08:56:46 +0600

    WhileLoops

---
+++ Loooppps.py
@@ -0,0 +1,44 @@
+#looping
+#loop over array and str
+# fruits = ['a', 'b', 'c']
+
+# for x in fruits:
+#     print(fruits)
+
+# for x in fruits:
+#     if x =='a':
+#         print("True")
+
+# for x in fruits:
+#     if x =='c':
+#         print("True from b")
+#     if x =='b':
+#         print("True from c")
+
+gretting = "hello"
+for x in gretting:
+    print(x)
+
+#check if str contains vowel
+gretting = "hello"
+for x in gretting:
+    if x == "e":
+        print(x)
+
+#looping for a specifice times using RANGE
+for x in range(3):
+    print ("pala")
+
+#loop 
+for x in range(3):
+    print ("pala")
+    print("wanghcuk")
+    print("-----------")
+
+#specify like print 5 till 13
+for i in range(5,13):
+    print(i)
+
+#lopping only even number
+for i in range(0,10, 2):
+    print(i)
---
+++ whileLoop.py
@@ -0,0 +1,38 @@
+#we use while loop unilt condition are met
+#return true or fALSE
+# it keeps on running until the conditon is true
+
+count = 0
+while count < 1:
+    print(count)
+    count = count + 1 #output 0 1 2 3 4 
+
+#user input str is unknown
+#counter --
+p = "peldenwangchuk"
+counter = 0
+lenght_of_p = len(p)
+print("counter is :", counter)
+print("lenght_of_p :", lenght_of_p)
+
+print('going in loop')
+while counter < lenght_of_p:
+    print("counter:", counter)
+    char = p[counter]
+    print(char)
+    counter = counter + 1 #output p, e, l ,d.........
+
+
+p = "peldenwangchuk"
+counter = 0
+lenght_of_p = len(p)
+print("counter is :", counter)
+print("lenght_of_p :", lenght_of_p)
+
+print('going in loop')
+while counter < lenght_of_p:
+    print("counter:", counter)
+    char = p[counter]
+    print(char)
+    counter = counter + 1
+    print("-------")
